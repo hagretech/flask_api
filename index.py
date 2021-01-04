0923be7519a09f004db8da9193b9e47902784b28
kirubel demelah <kiztopia.2020@gmail.com>
2021-01-04 10:56:55 -0500

    videos are added in the dect using put methond

--- index.py
+++ index.py
@@ -1,26 +1,32 @@
 from flask import Flask
-from flask_restful import Api, Resource, reqparse
+from flask_restful import Api, abort, Resource, reqparse
 
 app = Flask(__name__)
 api = Api(app)
 
-vid_args = reqparse.RequestParser()
-vid_args.add_argument("name", type='str', help='you need to add the name', required=True)
-vid_args.add_argument("likes", type='int', help='you need to add the likes', required=True)
-vid_args.add_argument("view", type='int', help='you need to add the views', required=True)
-
+vid_put_args = reqparse.RequestParser()
+vid_put_args.add_argument('name', type=str, help='nave is not found', required=True)
+vid_put_args.add_argument('likes', type=int, help='likes is not found', required=True)
+vid_put_args.add_argument('views', type=int, help='views are not found', required=True)
 videos = {}
+
+def if_vid_exit(vid_id):
+    if vid_id not in videos:
+        abort(404, message='video doesn\'t exist')
+
 
 
 class videosRoute(Resource):
     def get(self, vid_id):
+        if_vid_exit(vid_id)
         return videos[vid_id]
 
     def put(self, vid_id):
-        args = vid_args.parse_args()
-        return {args}
+        args = vid_put_args.parse_args()
+        videos[vid_id] = args
+        return videos[vid_id]
 
-
-api.add_resource(videosRoute, '/<int:vid_id>')
+ 
+api.add_resource(videosRoute, '/video/<int:vid_id>')
 if __name__ == "__main__":
     app.run(debug=True)
--- test.py
+++ test.py
@@ -1,6 +1,6 @@
 import requests
 
-BASE = 'http://127.0.0.1:5000/1'
+BASE = 'http://127.0.0.1:5000/video/3'
 
-response = requests.put(BASE , {'name': 'kira', 'likes': 10, 'views': 100})
+response = requests.get(BASE , {'name':'kira2', 'likes': 176870, 'views': 145000})
 print(response.json())
